d8dfd759d18bd9e9a911079eab3f719627768399
WreetSarker <wreet.sarker@gmail.com>
2021-04-10 22:40:38 +0600

    added make appointment section in home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeaturedService from '../FeaturedService/FeaturedService';
 import Header from '../Header/Header';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Header></Header>
             <Services></Services>
             <FeaturedService></FeaturedService>
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
