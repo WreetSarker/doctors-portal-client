13492e8cbb932bd820d23001aeb646f0cf227855
WreetSarker <wreet.sarker@gmail.com>
2021-04-11 15:57:01 +0600

    added appointment component and moved navbar and footer to shared component

---
+++ src/components/Appointment/Appointment/Appointment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../Shared/Footer/Footer'
+import NavBar from '../../Shared/NavBar/NavBar';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1>This is Appointment</h1>
+            <NavBar></NavBar>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Appointment;
