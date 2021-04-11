d82db5f6d8e940205d0b7a8e4c6620951fe2ef74
WreetSarker <wreet.sarker@gmail.com>
2021-04-12 00:07:48 +0600

    added login and private route components with google authentication in firebase

---
+++ src/components/Login/Login/Login.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useHistory, useLocation } from 'react-router';
+import { UserContext } from '../../../App';
+import LoginBg from '../../../images/loginBg.png'
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+} else {
+    firebase.app()
+}
+
+const Login = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                const credential = result.credential;
+                const token = credential.accessToken;
+                const user = result.user;
+                setLoggedInUser(user);
+                history.replace(from);
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.email;
+                const credential = error.credential;
+            });
+    }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+            .then(function (idToken) {
+                sessionStorage.setItem('token', idToken);
+                history.replace(from);
+            }).catch(function (error) {
+                // Handle error
+            });
+    }
+
+
+    return (
+        <div className="login-page container">
+            <div className="row align-items-center" style={{ height: "100vh" }}>
+                <div className="col-md-6 shadow p-5">
+                    <div className="form-group">
+                        <label htmlFor="">User Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Password</label>
+                        <input type="password" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="" className="text-danger">Forgot your password?</label>
+                    </div>
+                    <div className="from-group mt-5">
+                        <button className="btn btn-brand" onClick={handleGoogleSignIn}>Google Sign in</button>
+                    </div>
+                </div>
+                <div className="col-md-6 d-none d-md-block align-self-end">
+                    <img className="img-fluid" src={LoginBg} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDabya4HxeTz4E8_yBrJgLAhFW1R94OyMg",
+    authDomain: "doctors-portal-wreet.firebaseapp.com",
+    projectId: "doctors-portal-wreet",
+    storageBucket: "doctors-portal-wreet.appspot.com",
+    messagingSenderId: "798067729617",
+    appId: "1:798067729617:web:651a481d9db65c24f091f0"
+};
+
+export default firebaseConfig;
